6e73b70cba3dfc3b7f16cbcbcc6eb26a1261a307
Scott Waldron <waldros2@wwu.edu>
2019-06-11 13:32:21 -0700

    ERGM running with yaml

--- esRetrieval.py
+++ esRetrieval.py
@@ -14,16 +14,16 @@ try:
 except yaml.YAMLError as exc:
     print(exc)
 
-start_seq = config['start_date'].split('.')
+start_seq = config['elastic_search']['start_date'].split('.')
 start_date = datetime.datetime(int(start_seq[0]), int(start_seq[1]), int(start_seq[2]))
 
-end_seq = config['end_date'].split('.')
+end_seq = config['elastic_search']['end_date'].split('.')
 end_date = datetime.datetime(int(end_seq[0]),int(end_seq[1]),int(end_seq[2]))
 
 date_diff = end_date - start_date
 
 # #Establish ElasticSearch
-es = Elasticsearch(hosts=config['host_list'])
+es = Elasticsearch(hosts=config['elastic_search']['host_list'])
 
 #Establish indices for the days
 indices = []
@@ -34,14 +34,14 @@ print(indices)
 
 #Collect from days
 for index in indices:
-    print('Collecting ' + config['search_type'] + ' data from ' + index)
+    print('Collecting ' + config['elastic_search']['search_type'] + ' data from ' + index)
     connect_dict = defaultdict(float)
     try:
-        res = es.search(index=index, q=config['query'], size=10000, scroll='1m')
+        res = es.search(index=index, q=config['elastic_search']['query'], size=10000, scroll='1m')
         while len(res['hits']['hits']) > 0:
             for hit in res['hits']['hits']:
                 #Collecting netflow data
-                if(config['search_type'] == 'byte'):
+                if(config['elastic_search']['search_type'] == 'byte'):
                     netflow = hit['_source']['netflow']
             
                     #Establish src/dst
@@ -57,16 +57,16 @@ for index in indices:
                 
                     connect_dict[(src,dst)] += (netflow['out_bytes'] / 2**20)
                     connect_dict[(dst,src)] += (netflow['in_bytes'] / 2**20)
-                elif(config['search_type'] == 'dns'):
+                elif(config['elastic_search']['search_type'] == 'dns'):
                     src = hit['_source']['src_ip']
                     dst = hit['_source']['dest_ip']
                     connections[(src,dst)] += 1
 
             scroll = res['_scroll_id']
             res = es.scroll(scroll_id=scroll, scroll='1m')
-        with open(config['save_path'] + config['search_type']+ '_' + index + '.csv', 'w') as outfile:
+        with open(config['elastic_search']['save_path'] + config['elastic_search']['search_type']+ '_' + index + '.csv', 'w') as outfile:
             writer = csv.writer(outfile)
-            writer.writerow(['src', 'dst', config['search_type']])
+            writer.writerow(['src', 'dst', 'aggregation'])
             for key, value in connect_dict.items():
                     writer.writerow([key[0], key[1], value])
     except Exception as e:
