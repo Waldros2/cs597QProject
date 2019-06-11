9ff76db78e36a7bfc0694df15fa076ea8201f28f
Scott Waldron <waldros2@wwu.edu>
2019-06-11 12:25:57 -0700

    Addition of ElasticSearch retrieval with example YAML

--- esRetrieval.py
+++ esRetrieval.py
@@ -1,54 +1,74 @@
 import datetime
 import csv
+import sys
 from collections import defaultdict
 from elasticsearch import Elasticsearch
+import yaml
 
 #Establish date range
-index = 'logstash-2018.04.30'
-date = datetime.datetime(2018, 4, 30)
 
-#Establish ElasticSearch
-es = Elasticsearch()
+# Read in the YAML file
+YAML_file = open(sys.argv[1])
+try:
+    config = yaml.safe_load(YAML_file)
+except yaml.YAMLError as exc:
+    print(exc)
+
+start_seq = config['start_date'].split('.')
+start_date = datetime.datetime(int(start_seq[0]), int(start_seq[1]), int(start_seq[2]))
+
+end_seq = config['end_date'].split('.')
+end_date = datetime.datetime(int(end_seq[0]),int(end_seq[1]),int(end_seq[2]))
+
+date_diff = end_date - start_date
+
+# #Establish ElasticSearch
+es = Elasticsearch(hosts=config['host_list'])
 
 #Establish indices for the days
 indices = []
-for i in range(31):
-    indices.append('logstash-' + date.strftime('%Y.%m.%d'))
-    date += datetime.timedelta(days=1)
+while start_date <= end_date:
+    indices.append('logstash-' + start_date.strftime('%Y.%m.%d'))
+    start_date += datetime.timedelta(days=1)
+print(indices)
 
 #Collect from days
 for index in indices:
-    print('Collecting from ' + index)
-    byteFlow = defaultdict(float)
+    print('Collecting ' + config['search_type'] + ' data from ' + index)
+    connect_dict = defaultdict(float)
     try:
-        res = es.search(index=index, q='clientID : washougal AND _type: netflow', size=10000, scroll='1m')
+        res = es.search(index=index, q=config['query'], size=10000, scroll='1m')
         while len(res['hits']['hits']) > 0:
             for hit in res['hits']['hits']:
-                netflow = hit['_source']['netflow']
+                #Collecting netflow data
+                if(config['search_type'] == 'byte'):
+                    netflow = hit['_source']['netflow']
+            
+                    #Establish src/dst
+                    if('192.168' not in netflow['ipv4_src_addr']):
+                        src = 'superNode'
+                    else:
+                        src = netflow['ipv4_src_addr']
+                        
+                    if('192.168' not in netflow['ipv4_dst_addr']):
+                        dst = 'superNode'
+                    else:
+                        dst = netflow['ipv4_dst_addr']
                 
-                #Establish src/dst
-                if('192.168' not in netflow['ipv4_src_addr']):
-                    src = 'superNode'
-                else:
-                    src = netflow['ipv4_src_addr']
-                    
-                if('192.168' not in netflow['ipv4_dst_addr']):
-                    dst = 'superNode'
-                else:
-                    dst = netflow['ipv4_dst_addr']
-                
-                byteFlow[(src,dst)] += (netflow['out_bytes'] / 2**20)
-                byteFlow[(dst,src)] += (netflow['in_bytes'] / 2**20)
-                
+                    connect_dict[(src,dst)] += (netflow['out_bytes'] / 2**20)
+                    connect_dict[(dst,src)] += (netflow['in_bytes'] / 2**20)
+                elif(config['search_type'] == 'dns'):
+                    src = hit['_source']['src_ip']
+                    dst = hit['_source']['dest_ip']
+                    connections[(src,dst)] += 1
+
             scroll = res['_scroll_id']
             res = es.scroll(scroll_id=scroll, scroll='1m')
-        with open('../data/byte/byte_' + index + '.csv', 'w') as outfile:
+        with open(config['save_path'] + config['search_type']+ '_' + index + '.csv', 'w') as outfile:
             writer = csv.writer(outfile)
-            writer.writerow(['src','dst','MB'])
-            for key, value in byteFlow.items():
-                writer.writerow([key[0], key[1], value])
-
+            writer.writerow(['src', 'dst', config['search_type']])
+            for key, value in connect_dict.items():
+                    writer.writerow([key[0], key[1], value])
     except Exception as e:
         print('Could not collect from ' + index)
         print(e)
-
